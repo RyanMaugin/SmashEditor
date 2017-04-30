0ce8cf47848ccddcbead0eb865d4a0efba3bea20
RyanMaugin <ryanmauginn@gmail.com>
2017-04-30 14:28:42 +0100

    Added AlertUser Method in EditorFileHandler Class & Used it for Error handling in Rename Method

--- src/app/classes/EditorFileHandler.java
+++ src/app/classes/EditorFileHandler.java
@@ -1,5 +1,6 @@
 package app.classes;
 
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.web.HTMLEditor;
 import javafx.stage.FileChooser;
@@ -84,23 +85,50 @@ public class EditorFileHandler {
      */
     public static void rename() {
 
-        // Create new text input dialog to prompt user for new file name
-        TextInputDialog renameDialog = new TextInputDialog("newname");
-        renameDialog.setTitle("Rename File");
-        renameDialog.setHeaderText("Enter New Name");
-        renameDialog.setContentText("Enter new file name: ");
+        if (savePath != null) {
+            // Create new text input dialog to prompt user for new file name
+            TextInputDialog renameDialog = new TextInputDialog("newname");
+            renameDialog.setTitle("Rename File");
+            renameDialog.setHeaderText("Enter New Name");
+            renameDialog.setContentText("Enter new file name: ");
 
-        // Get new file name response from text input dialog
-        Optional<String> result = renameDialog.showAndWait();
-        result.ifPresent(name -> {
-            // Rename the file with response name
-            File oldFile = new File(savePath);
-            File newFile = new File(oldFile.getParent() + "/" + name);
+            // Get new file name response from text input dialog
+            Optional<String> result = renameDialog.showAndWait();
+            result.ifPresent(name -> {
+                // Rename the file with response name
+                File oldFile = new File(savePath);
+                File newFile = new File(oldFile.getParent() + "/" + name);
 
-            // Rename file and handle any error
-            if (oldFile.renameTo(newFile)) System.out.println("File renamed!");
-            else System.out.println("Error file was not renamed!");
-        });
+                // Rename file and handle any error
+                if (oldFile.renameTo(newFile)) System.out.println("File renamed!");
+                else alertUser(Alert.AlertType.WARNING, "Warning", "Cannot leave file name empty", "Please enter a name for file!");
+            });
+
+        // If there is no open file to rename then show a warning alert telling the user
+        } else {
+            alertUser(
+                    Alert.AlertType.WARNING,
+                    "Cannot Rename Non-existent File",
+                    "No File Opened",
+                    "You need to open a file or create a new file in order to rename!"
+            );
+        }
+    }
+
+
+    /**
+     * This is an alert method
+     * @param alertType
+     * @param title
+     * @param headerText
+     * @param contentText
+     */
+    public static void alertUser(Alert.AlertType alertType, String title, String headerText, String contentText) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.setContentText(contentText);
+        alert.showAndWait();
     }
 
 }

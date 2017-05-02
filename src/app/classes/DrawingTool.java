ecb1e579a868c95d576abb342d79560113eafb13
RyanMaugin <ryanmauginn@gmail.com>
2017-05-02 10:23:17 +0100

    Added DrawTool tool bar

--- src/app/classes/DrawingTool.java
+++ src/app/classes/DrawingTool.java
@@ -1,7 +1,10 @@
 package app.classes;
 
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
+import javafx.scene.control.Button;
+import javafx.scene.control.Separator;
+import javafx.scene.control.ToolBar;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -25,13 +28,36 @@ public class DrawingTool {
 
         // Create VBox for stacking elements at top of border pane without overlap
         VBox verticalBox = new VBox();
-        verticalBox.getChildren().addAll();
+        verticalBox.getChildren().addAll(
+                // Add all components here
+                drawToolBar()
+        );
         root.setTop(verticalBox);
 
         // Configure stage
         drawToolStage.setTitle("Smash Editor - Draw Tool");
         drawToolStage.setScene(new Scene(root, 700, 504));
         drawToolStage.show();
+    }
+
+
+    /**
+     * Draw Tool Bar
+     * @return draw tool bar
+     */
+    public static ToolBar drawToolBar() {
+        ToolBar drawToolBar = new ToolBar();
+
+        // Create elements for tool bar
+        Button saveButton = new Button("Save");
+        Separator saveSeparator = new Separator(Orientation.VERTICAL);
+        Button undoButon = new Button("Undo");
+        Button redoButton = new Button("Redo");
+
+        // Add elements to tool bar
+        drawToolBar.getItems().addAll(saveButton, saveSeparator, undoButon, redoButton);
+
+        return drawToolBar;
     }
 
 }
